416b1e3216587bd2a8a380bf218ebf5c101eebd3
MUHAMMED FURKAN INALCIK <e2171643@cenginek>
2018-03-12 23:37:02 +0300

    initialized the1_v2.asm

---
+++ the1_v2.asm
@@ -0,0 +1,159 @@
+nclude "p18f8722.inc"
+; CONFIG1H
+  CONFIG  OSC = HSPLL, FCMEN = OFF, IESO = OFF
+; CONFIG2L
+  CONFIG  PWRT = OFF, BOREN = OFF, BORV = 3
+; CONFIG2H
+  CONFIG  WDT = OFF, WDTPS = 32768
+; CONFIG3L
+  CONFIG  MODE = MC, ADDRBW = ADDR20BIT, DATABW = DATA16BIT, WAIT = OFF
+; CONFIG3H
+  CONFIG  CCP2MX = PORTC, ECCPMX = PORTE, LPT1OSC = OFF, MCLRE = ON
+; CONFIG4L
+  CONFIG  STVREN = ON, LVP = OFF, BBSIZ = BB2K, XINST = OFF
+; CONFIG5L
+  CONFIG  CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF, CP4 = OFF, CP5 = OFF
+  CONFIG  CP6 = OFF, CP7 = OFF
+; CONFIG5H
+  CONFIG  CPB = OFF, CPD = OFF
+; CONFIG6L
+  CONFIG  WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF, WRT4 = OFF
+  CONFIG  WRT5 = OFF, WRT6 = OFF, WRT7 = OFF
+; CONFIG6H
+  CONFIG  WRTC = OFF, WRTB = OFF, WRTD = OFF
+; CONFIG7L
+  CONFIG  EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF, EBTR4 = OFF
+  CONFIG  EBTR5 = OFF, EBTR6 = OFF, EBTR7 = OFF
+; CONFIG7H
+  CONFIG  EBTRB = OFF
+
+;*******************************************************************************
+; Variables & Constants
+;*******************************************************************************
+UDATA_ACS
+  t1	res 1	; used in delay
+  t2	res 1	; used in delay
+  t3	res 1	; used in delay
+  state res 1	; controlled by RB0 button
+;*******************************************************************************
+; Reset Vector
+;*******************************************************************************
+
+RES_VECT  CODE    0x0000            ; processor reset vector
+    GOTO    START                   ; go to beginning of program
+
+;*******************************************************************************
+; MAIN PROGRAM
+;*******************************************************************************
+
+MAIN_PROG CODE	; let linker place main program
+
+
+START
+    call INIT	; initialize variables and ports
+    call DELAY	; wait a second
+
+Pos1
+    BCF LATA, 2
+    BSF LATA, 0
+    BSF LATA, 1
+    call DELAY	; wait a second
+    
+
+Pos2
+    BCF LATA, 0
+    BSF LATA, 1
+    BSF LATA, 2
+    call DELAY	; wait a second
+    
+
+MAIN_LOOP
+    
+    CALL Pos1
+
+    call DELAY	; wait a second
+
+	CALL Pos2
+
+    GOTO MAIN_LOOP  ; loop forever
+
+
+DELAY	; Time Delay Routine with 3 nested loops
+    MOVLW 18	; Copy desired value to W
+    MOVWF t3	; Copy W into t3
+    _loop3:
+	MOVLW 0xA0  ; Copy desired value to W
+	MOVWF t2    ; Copy W into t2
+	_loop2:
+	    MOVLW 0x9F	; Copy desired value to W
+	    MOVWF t1	; Copy W into t1
+	    _loop1:
+		decfsz t1,F ; Decrement t1. If 0 Skip next instruction
+		GOTO _loop1 ; ELSE Keep counting down
+		decfsz t2,F ; Decrement t2. If 0 Skip next instruction
+		GOTO _loop2 ; ELSE Keep counting down
+		decfsz t3,F ; Decrement t3. If 0 Skip next instruction
+		GOTO _loop3 ; ELSE Keep counting down
+		return
+
+
+INIT
+
+MOVLW 0Fh
+MOVWF ADCON1
+
+    CLRF    state
+    MOVLW   0xFF
+    CLRF    LATB
+
+    MOVWF   LATC
+    MOVWF   LATD
+    MOVWF   LATE
+    MOVWF   LATF
+    CLRF    LATG
+    MOVWF   LATH    ; selects display permutation
+    MOVWF   LATJ    ; on/off segment in a display
+
+    MOVLW   0xFF
+    MOVWF   TRISB
+    CLRF    TRISA
+    CLRF    TRISB
+    CLRF    TRISC
+    CLRF    TRISD
+
+    return
+
+
+BUTTON_TASK ; very primitive button task
+    BTFSS PORTB,0
+    return
+
+    _debounce:
+	BTFSC PORTB,0
+	goto _debounce	; busy waiting. FIXME !!!
+
+
+
+
+	COMF state, 1
+
+	return
+
+
+TABLE
+    MOVF    PCL, F  ; A simple read of PCL will update PCLATH, PCLATU
+    RLNCF   WREG, W ; multiply index X2
+    ADDWF   PCL, F  ; modify program counter
+    RETLW b'00111111' ;0 representation in 7-seg. disp. portJ
+    RETLW b'00000110' ;1 representation in 7-seg. disp. portJ
+    RETLW b'01011011' ;2 representation in 7-seg. disp. portJ
+    RETLW b'01001111' ;3 representation in 7-seg. disp. portJ
+    RETLW b'01100110' ;4 representation in 7-seg. disp. portJ
+    RETLW b'01101101' ;5 representation in 7-seg. disp. portJ
+    RETLW b'01111101' ;6 representation in 7-seg. disp. portJ
+    RETLW b'00000111' ;7 representation in 7-seg. disp. portJ
+    RETLW b'01111111' ;8 representation in 7-seg. disp. portJ
+    RETLW b'01100111' ;9 representation in 7-seg. disp. portJ
+
+
+END
